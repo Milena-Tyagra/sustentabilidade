b018f33a51db52f2e2536901f9313a67f8570cba
Milena Tyagra <milenatyagra.sc@gmail.com>
2025-08-25 20:36:50 -0300

    ajustes de responsividade

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,36 +1,61 @@
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { List } from "@phosphor-icons/react";
 
 import { Sidebar } from "./Sidebar";
 
 import { messages } from "../../helpers/messages";
 
+const useWindowSize = () => {
+  const [size, setSize] = useState([window.innerWidth, window.innerHeight])
+
+  useLayoutEffect(() => {
+      function updateSize() {
+          setSize([window.innerWidth, window.innerHeight])
+      }
+
+      window.addEventListener('resize', updateSize)
+      updateSize()
+      return () => window.removeEventListener('resize', updateSize)
+  }, [])
+
+  return size
+}
 export const Layout = ({ children }) => {
+  const [windowWidth, windowHeight] = useWindowSize()
+  const maxWidthMobileMode = 700 //px
+  const mobile_mode = windowWidth <= maxWidthMobileMode
+
   const [sidebar, setSidebar] = useState(false);
-  const [contentAlwaysVisible, setContentAlwaysVisible] = useState(false);
+  const [contentAlwaysVisible, setContentAlwaysVisible] = useState(!mobile_mode);
+
+  const headerHeigth = document.getElementById('header')?.offsetHeight || 0
+  const footerHeigth = document.getElementById('footer')?.offsetHeight || 0
+
+  const height = windowHeight - headerHeigth - footerHeigth
+  console.log(height)
 
   return (
-    <div className="bg-secondary-100 w-screen min-h-screen  flex flex-col">
-      <div className=" w-full py-3 bg-secondary-200 text-xl">
-        <div className="font-script text-primary-200 flex flex-col items-center justify-center ">
-          <List
+    <div className="bg-secondary-100 w-screen h-full flex flex-col">
+      <div className="w-full py-3 bg-secondary-200 text-xl relative" id="header">
+        <div className="font-script text-primary-200 flex flex-col items-center justify-center" >
+          {mobile_mode && <List
             size={32}
             className="absolute top-1 left-1 cursor-pointer"
             onClick={() => setSidebar((current) => !current)}
-          />
+          />}
           <span>{messages.TITLE}</span>
           <span>{messages.SUBTITLE}</span>
         </div>
       </div>
-      <div className="flex-1 w-full flex">
+      <div className="flex-1 w-full flex h-full">
         {(sidebar || contentAlwaysVisible) && (
-          <Sidebar onClick={() => setSidebar(false)} />
+          <Sidebar onClick={() => setSidebar(false)} mobile={mobile_mode} height={height}/>
         )}
         {(!sidebar || contentAlwaysVisible) && (
-          <div className="p-4 font-sans text-gray-700">{children}</div>
+          <div className={`p-4 md:px-12 font-sans text-gray-700 h-[${height}px] overflow-auto`}>{children}</div>
         )}
       </div>
-      <div className=" w-full py-3 bg-secondary-200 text-xs">
+      <div className=" w-full py-3 bg-secondary-200 text-xs" id="footer">
         <div className="font-sans text-primary-200 flex flex-col items-center justify-center ">
           <p>
             &copy;
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,15 +1,15 @@
 import { NavLink } from "react-router-dom";
 
-export const Sidebar = ({onClick}) => {
+export const Sidebar = ({onClick, mobile, height}) => {
     const pages = [
         { title: 'Home', path: '/' },
         { title: 'Artesanato', path: '/art' },
         { title: 'Receitas', path: '/recepy' },
     ]
     return (
-        <div className="flex flex-col gap-1 w-full h-full"> 
+        <div className={`flex flex-col gap-3 px-2  ${mobile ? ' w-full bg-secondary-100 py-3' : 'min-w-[200px] bg-secondary-200'}`} style={{height}}> 
         {pages.map((page, i) => <ul key={`page-${i}`}>
-                    <li className="text-center w-full font-script text-primary-200 text-xl hover:bg-secondary-200 rounded" onClick={onClick} >
+                    <li className={`text-center w-full font-script text-primary-200 text-xl ${mobile ? 'hover:bg-secondary-200' : 'hover:bg-secondary-100'}  `} onClick={onClick} >
                         <NavLink to={page.path}>
                             <span >{page.title}</span>
                         </NavLink>
