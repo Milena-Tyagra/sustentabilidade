eae52506303066e01520f24b0e4d8a21d1c6bde5
~Milena <~solvel.milena.t@gmail.com~>
2025-08-27 20:24:46 -0300

    finalizando home com banner e ajustes gerais

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,54 +1,66 @@
-import { 
-    Leaf,
-    Tree,
-    UsersThree,
-    CurrencyCircleDollar 
+import {
+  Leaf,
+  Tree,
+  UsersThree,
+  CurrencyCircleDollar
 } from "@phosphor-icons/react";
 import { messages } from "../../helpers/messages";
 import { products_examples } from "../../helpers/mocks";
 
+import banner from '../../assets/banner.jpeg'
+import { useContext } from "react";
+import { Context } from "../../context/Context";
+
 export const Home = () => {
-    
-const icon_classes ="w-8 h-8 md:w-4 md:h-4 shrink-0 text-primary-200"
+  const { mobile_mode } = useContext(Context)
+  const icon_classes = "w-8 h-8 md:w-6 md:h-6 shrink-0 text-primary-200"
   const impacts = [
     {
-        icon: <Tree className={icon_classes} />,
-        text: messages.BENEFIT_1
+      icon: <Tree className={icon_classes} />,
+      text: messages.BENEFIT_1
     },
     {
-        icon: <UsersThree className={icon_classes} />,
-        text: messages.BENEFIT_2
+      icon: <UsersThree className={icon_classes} />,
+      text: messages.BENEFIT_2
     },
     {
-        icon: <CurrencyCircleDollar  className={icon_classes} />,
-        text: messages.BENEFIT_3
+      icon: <CurrencyCircleDollar className={icon_classes} />,
+      text: messages.BENEFIT_3
     }
   ]
 
   return (
-    <div className="flex flex-col gap-3 font-sans ">
+    <div className="flex flex-col gap-5 font-sans ">
+      <div className="w-full flex justify-center">
+        <img src={banner} style={{ width: mobile_mode ? "90vw" : "40vw" }} />
+      </div>
+
       <p>{messages.PROBLEM}</p>
       <p>{messages.SOLUTION}</p>
 
-      <b className="text-center text-primary-200">{messages.PRODUCTS_EXAMPLES}</b>
 
       <div>
+        <div className="flex justify-center items-center mb-1">
+          <b className="text-center text-primary-200">{messages.PRODUCTS_EXAMPLES}</b>
+        </div>
         {products_examples.map((example, i) => (
           <span key={`example-${i}`} className="flex items-center gap-1.5">
-          <Leaf className="w-4 h-4 shrink-0 text-primary-200" />
-          <span className="text-sm">{example}</span>
-        </span>
+            <Leaf className="w-4 h-4 md:w-5 md:h-5 shrink-0 text-primary-200" size={30} />
+            <span>{example}</span>
+          </span>
         ))}
       </div>
 
-      <b className="text-center text-primary-200">{messages.IMPACTS}</b>
 
       <div>
-      {impacts.map((impact, i) => (
+        <div className="flex justify-center items-center mb-1">
+          <b className="text-center text-primary-200">{messages.IMPACTS}</b>
+        </div>
+        {impacts.map((impact, i) => (
           <span key={`impact-${i}`} className="flex items-center gap-1.5">
-          {impact.icon}
-          <span className="text-sm">{impact.text}</span>
-        </span>
+            {impact.icon}
+            <span >{impact.text}</span>
+          </span>
         ))}
       </div>
 
