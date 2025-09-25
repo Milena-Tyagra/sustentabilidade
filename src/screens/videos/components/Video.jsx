70ee19b1400d33017515489cbf8a928c668e1349
~Milena <~solvel.milena.t@gmail.com~>
2025-09-24 23:45:22 -0300

    lidando com troca de video para iframe

--- src/screens/videos/components/Video.jsx
+++ src/screens/videos/components/Video.jsx
@@ -4,15 +4,12 @@ import { Context } from "../../../context/Context"
 import { Spin } from "antd"
 
 export const Video = ({ video }) => {
-    const { mobile_mode, windowWidth } = useContext(Context)
-    const [loaded, setLoaded] = useState(false);
+    const { mobile_mode, windowWidth, windowHeight } = useContext(Context)
 
-    const videosWidth = windowWidth * (mobile_mode ? 0.9 : 0.5)
+    const videosWidth = windowWidth * (mobile_mode ? 0.8 : 0.5)
+    const videosHeight = windowHeight * (mobile_mode ? 0.4 : 0.5)
     return <div className="w-full">
         <Separator title={video.title} />
-        {(!video || !loaded) && <div className="w-full min-h-[200px] h-full flex items-center justify-center">
-            <Spin />
-        </div>}
-        <iframe width="560" height="315" src={video.media} title={`Vídeo ${video.title}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width={videosWidth} height={videosHeight} src={video.media} title={`Vídeo ${video.title}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 }
